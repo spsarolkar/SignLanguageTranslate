28db72724f11f17fccbcbab9e3fc6c84e1d11186
SunilS <spsarolkar@gmail.com>
2026-01-03 21:49:52 +0530

    Initial Commit

---
+++ SignLanguageTranslate/SignLanguageTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignLanguageTranslateApp.swift
+//  SignLanguageTranslate
+//
+//  Created by SunilS on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SignLanguageTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
